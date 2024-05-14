245aeafe66e82a23123dac21ab0d318a3a96e994
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2024-05-14 13:46:34 +0200

    refactor: Update read_item function to handle item not found

--- app/routers/items.py
+++ app/routers/items.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
 from typing import Union
 
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+    "test": {"id": "test", "title": "Test", "description": "This is a test"},
+}
+
 router = APIRouter(prefix="/items", tags=["items"],responses={404: {"description": "Not found"}})
 
 @router.get("/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str):
+    if item_id not in fake_db:
+        return {"message": "Item not found"}
+    return fake_db[item_id]
