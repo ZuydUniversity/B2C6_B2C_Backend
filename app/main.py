664a7f0832ac968feb3e4c78a2cea3a38759f8a7
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2024-05-15 12:59:36 +0200

    chore: Update FastAPI middleware to allow CORS for all origins

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from .routers import items
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(items.router)
 
