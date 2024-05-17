628845deae9256d3e0b98515536a6fea328842e9
David Beljaars <davidbel.zakelijk@gmail.com>
2024-05-17 20:24:13 +0200

    added live database
    de database draait nu live op een ubuntu VM in azure. Aangezien het niet mogelijk was om in azure een mariadb te maken moest het op deze manier.
    
    Stappen doornemen dit te bereiken:
    
    - UbuntuVM opgezet met custom port regels zodat 3306 wordt toegestaan(dit is voor remote access)
    
    - MariaDB geinstalleerd op deze VM en in de config files : bind-address = 0.0.0.0 neergezet zodat remote access mogelijk is
    
    - een user aangemaakt die alle permissies heeft op de database(zie connection string)
    
    - connection string aangepast naar live database(ip adres van de VM en user met databasenaam

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Newfuture1@host.docker.internal/mvpdb"
+DATABASE_URL = "mysql+pymysql://basicuser:admin@172.205.200.109:3306/mvpdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
