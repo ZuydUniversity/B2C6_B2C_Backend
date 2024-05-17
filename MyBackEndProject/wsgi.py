4814dc6bb78145a53d14c3145ca48dfa18d90e5e
Tristan Franssen <tristanfranssen@gmail.com>
2024-05-17 12:52:42 +0200

    Made code ready for azure

--- MyBackEndProject/wsgi.py
+++ MyBackEndProject/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBackEndProject.settings')
+settings_module = 'MyBackEndProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'MyBackEndProject.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBackEndProject.settings')
+    settings_module = 'MyBackEndProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'MyBackEndProject.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
