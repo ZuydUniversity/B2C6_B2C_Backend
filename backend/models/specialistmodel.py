f91a5cdf1f170f0f50b32b4542cb99f4b92276b7
Noah <Noah@LAPTOPPIE>
2024-06-06 22:47:35 +0200

    Changed some importing things

--- backend/models/specialistmodel.py
+++ backend/models/specialistmodel.py
@@ -2,12 +2,11 @@
 Contains models that store data
 '''
 from typing import List
-from . import usermodel, notemodel, sessionmodel, appointmentmodel
+from . import usermodel
 
 class Specialist(usermodel.User):
     '''Class for the specialists. This is an Inherits from Usermodel '''
     PatientIds: List[int]
-    Notes: List[notemodel.Note]
-    Sessions: List[sessionmodel.Session]
-    Appointments: List[appointmentmodel.Appointment]
-    
+    NoteIds: List[int]
+    SessionsId: List[int]
+    AppointmentsIds: List[int]
