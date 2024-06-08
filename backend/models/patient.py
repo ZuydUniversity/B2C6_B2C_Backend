a5cfd1271d467bb596d85b03cdb6e01dbe5891bf
jouryl00 <loojoury@gmail.com>
2024-06-08 14:25:10 +0200

    Update patient.py

--- backend/models/patient.py
+++ backend/models/patient.py
@@ -1,9 +1,10 @@
 """
 This module defines the Patient model for the database.
-Each Patient has an id, name, age, and gender.
 """
+
 from ..database import Base
 from sqlalchemy import Column, Integer, String
+#from sqlalchemy.orm import relationship
 
 class Patient(Base):
     __tablename__ = "patients"
@@ -12,3 +13,4 @@ class Patient(Base):
     name = Column(String(100), index=True)
     age = Column(Integer)
     gender = Column(String(10))
+    #notes = relationship("Note", back_populates="patient")
