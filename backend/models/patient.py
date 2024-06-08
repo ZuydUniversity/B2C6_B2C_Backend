3bbf8cca2d5a9ce5ccfbd755c9a71a06917ae9ed
jouryl00 <loojoury@gmail.com>
2024-06-08 14:44:33 +0200

    pylint problems

--- backend/models/patient.py
+++ backend/models/patient.py
@@ -1,10 +1,9 @@
 """
 This module defines the Patient model for the database.
 """
-
-from ..database import Base
 from sqlalchemy import Column, Integer, String
-#from sqlalchemy.orm import relationship
+from ..database import Base
+
 
 class Patient(Base):
     __tablename__ = "patients"
@@ -13,4 +12,3 @@ class Patient(Base):
     name = Column(String(100), index=True)
     age = Column(Integer)
     gender = Column(String(10))
-    #notes = relationship("Note", back_populates="patient")
