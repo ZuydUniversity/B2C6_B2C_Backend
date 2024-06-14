9c84f93330de1f4e146acbde2a7d3cf2a4ce9e33
Noah <Noah@LAPTOPPIE>
2024-06-14 10:11:49 +0200

    Added the router logic. (This still need connection to Database.)

--- backend/routers/noterouter.py
+++ backend/routers/noterouter.py
@@ -2,13 +2,22 @@
 The router for the user which allows the user to register, 
 login, logout and forgotpassword functionalities
 '''
+from requests import Request
 from ..common import create_router
+from ..models.notemodel import Note
 
 router = create_router()
 
-@router.post("/notes/create")
-async def create_note():
+@router.post("/notes")
+async def create_note(request: Request):
     '''
     Creates a note
     '''
-    pass
+    data = await request.json()
+    name = data.get('name')
+    session_id = data.get('sessionId')
+    patient_id = data.get('patientId')
+    specialist_id = data.get('specialistId')
+
+    Note(Name=name, SessionId=session_id, PatientId=patient_id, SpecialistId=specialist_id)
+    # Here the rest of the code is placed to save to the database.
