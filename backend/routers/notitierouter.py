73b7888c9c6a5c94c551bec27c48090037cdb039
Noah <Noah@LAPTOPPIE>
2024-06-14 09:53:21 +0200

    added Notitie Router

---
+++ backend/routers/notitierouter.py
@@ -0,0 +1,7 @@
+'''
+The router for the user which allows the user to register, 
+login, logout and forgotpassword functionalities
+'''
+from ..common import create_router
+
+router = create_router()
