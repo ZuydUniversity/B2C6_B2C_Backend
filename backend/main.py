1c45c05e0f91dc9eb17e89128e0231ac07b27d9f
Tristan Franssen <tristanfranssen@gmail.com>
2024-06-21 11:43:14 +0200

    Fixed main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
+'''
+Entry file of fastapi project
+'''
 import ssl
 import os
-import base64
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -10,17 +12,11 @@ routers = [homerouter, userrouter]
 
 app = FastAPI()
 
-ssl_cert_b64 = os.getenv('SSL_CERT')
-ssl_key_b64 = os.getenv('SSL_KEY')
+ssl_cert = os.getenv('SSL_CERT')
+ssl_key = os.getenv('SSL_KEY')
 
-if ssl_cert_b64 is None or ssl_key_b64 is None:
+if ssl_cert is None or ssl_key is None:
     raise ValueError("SSL certificates not found in environment variables")
-
-try:
-    ssl_cert = base64.b64decode(ssl_cert_b64)
-    ssl_key = base64.b64decode(ssl_key_b64)
-except base64.binascii.Error as e:
-    raise ValueError("Base64 decoding error:", e)
 
 ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_context.load_cert_chain(certfile=ssl_cert, keyfile=ssl_key)
@@ -33,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-for router in routers:
-    app.include_router(router.router)
+for i in routers:
+    app.include_router(i.router)
 
 app.debug = True
