a78f8738ef4e3080300b7752f65fb5694ad2d4fc
Noah <Noah@LAPTOPPIE>
2024-06-21 12:09:46 +0200

    Added basic router test structure and started redoing router exceptions

--- backend/routers/noterouter.py
+++ backend/routers/noterouter.py
@@ -3,8 +3,10 @@ The router for the user which allows the user to register,
 login, logout and forgotpassword functionalities
 '''
 from requests import Request
+from sqlalchemy.exc import IntegrityError, OperationalError, DataError, DatabaseError
 from ..common import create_router
 from ..models.notemodel import Note
+
 
 router = create_router()
 
@@ -18,16 +20,25 @@ async def create_note(request: Request):
     session_id = data.get('sessionId')
     patient_id = data.get('patientId')
     specialist_id = data.get('specialistId')
-    if data.get('debug') is None or data.get('debug') is False:
-        debug = False
-    else:
-        debug = True
-
     note = Note(Name=name, SessionId=session_id, PatientId=patient_id, SpecialistId=specialist_id)
-    if debug is not True:
-        # Here the rest of the code is placed to save to the database.
-        pass
-    return note
+    print(note) # This is temporary to satisfy PyLint
+    message =  {"success": False, "error": "An unexpected error occurred"}
+    try:
+        # Functie die opslaan naar de database doet
+        message = {"success": True, "result": "Note created successfully"}
+    except IntegrityError as e:
+        # Specifieke afhandeling voor IntegrityError
+        message = {"success": False, "error": "IntegrityError: " + str(e)}
+    except OperationalError as e:
+        # Specifieke afhandeling voor OperationalError
+        message = {"success": False, "error": "OperationalError: " + str(e)}
+    except DataError as e:
+        # Specifieke afhandeling voor DataError
+        message = {"success": False, "error": "DataError: " + str(e)}
+    except DatabaseError as e:
+        # Specifieke afhandeling voor andere database fouten
+        message = {"success": False, "error": "DatabaseError: " + str(e)}
+    return message
 
 @router.patch("/notes/{note_id}")
 async def patch_note(request: Request, note_id: int):
@@ -42,7 +53,14 @@ async def patch_note(request: Request, note_id: int):
     note.SessionId = data.get('sessionId')
     note.PatientId = data.get('patientId')
     note.SpecialistId = data.get('specialistId')
-    # Here the rest of the code is placed to save to the database.
+    if data.get('debug') is None or data.get('debug') is False:
+        debug = False
+    else:
+        debug = True
+    if debug is not True:
+        # Here the rest of the code is placed to save to the database.
+        pass
+    return note
 
 @router.get('/notes')
 async def get_notes():
@@ -51,6 +69,8 @@ async def get_notes():
     '''
     # Code here that gets all notes
     note = Note()
+    note.Id = 1
+    note.Name = "test"
     return note
 
 @router.get('/notes/{note_id}')
@@ -61,6 +81,7 @@ async def get_note(note_id: int):
     # Code here that gets a note
     print(note_id) # This is temporary to satisfy PyLint
     note = Note()
+    note.Id = 1
     return note
 
 @router.delete('/notes/{note_id}')
@@ -70,3 +91,5 @@ async def delete_note(note_id: int):
     '''
     # Code here that deletes a note
     print(note_id) # This is temporary to satisfy PyLint
+    note = Note()
+    return note
--- backend/tests/noterouter_test.py
+++ backend/tests/noterouter_test.py
@@ -29,3 +29,35 @@ async def create_note_test():
     assert response.json()["patientId"] == test_note["patientId"]
     assert response.json()["sessionId"] == test_note["sessionId"]
     assert response.json()["specialistId"] == test_note["specialistId"]
+
+async def patch_note_test():
+    '''
+    Test if the patching of the notes works.
+    '''
+    response = client.patch("/api/notes/2", json=test_note)
+    assert response.status_code == 200
+    assert response.json()["name"] == test_note["id"]
+
+async def get_notes_test():
+    '''
+    Test if the getting of the notes works.
+    '''
+    response = client.get("/api/notes")
+    assert response.status_code == 200
+    assert response.json()[0]["id"] == 1
+    assert response.json()[1]["name"] == "test"
+
+async def get_note_test():
+    '''
+    Test if the getting of the note works.
+    '''
+    response = client.get("/api/notes/1")
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+
+async def delete_note_test():
+    '''
+    Test if the deleting of the notes works.
+    '''
+    response = client.delete("/api/notes/1")
+    assert response.status_code == 200
