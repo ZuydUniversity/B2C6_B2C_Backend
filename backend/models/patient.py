20a1fe6c20b5ba34d63266712e4d3a93994dc597
David Beljaars <davidbel.zakelijk@gmail.com>
2024-06-21 15:38:07 +0200

    removed spaces for pylint
    man what the hell is this

--- backend/models/patient.py
+++ backend/models/patient.py
@@ -3,7 +3,7 @@ This module contains the Patient model for the database.
 '''
 from sqlalchemy.ext.declarative import declarative_base # This should go in database.py once its made.
 from sqlalchemy import Column, ForeignKey, Integer, String, Table
-from sqlalchemy.orm import relationship 
+from sqlalchemy.orm import relationship
 
 Base = declarative_base() # This should go in database.py once its made.
 
@@ -11,13 +11,13 @@ class Patient(Base):
     __tablename__ = "patients"
 
     id = Column(Integer, primary_key=True)
-    firstName = Column(String(50))  
-    lastName = Column(String(50)) 
-    email = Column(String(100))  
+    firstName = Column(String(50))
+    lastName = Column(String(50))
+    email = Column(String(100))
     age = Column(Integer)
-    phonenumber = Column(Integer)  
-    gender = Column(String(10))  
-    contactpersonEmail = Column(String(100))  
+    phonenumber = Column(Integer)
+    gender = Column(String(10))
+    contactpersonEmail = Column(String(100))
     contactpersonPhonenumber = Column(Integer)
 
 # The relationships will be defined here. Models yet to be added so the relationships are commented out.
@@ -33,6 +33,6 @@ patient_medication_association = Table('patient_medication_association', Base.me
     Column('patient_id', Integer, ForeignKey('patients.id'), primary_key=True),
     Column('medication_id', Integer, ForeignKey('medication.id'), primary_key=True)
 )
-medication = relationship("Medication", secondary=patient_medication_association, back_populates="patients")   
+medication = relationship("Medication", secondary=patient_medication_association, back_populates="patients")
 
 appointments = relationship("Appointment", backref="patient")
