87e2ceaa5c4eca0aed327c0bbb5ef67f5b30c528
nurulgulpen <nurulgulpen@gmail.com>
2024-06-25 22:13:47 +0200

    adjusted sqlalchemy.orm.declarative_base()

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,7 @@ Module for database connection without Vault integration.
 
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # SQLAlchemy setup
 Base = declarative_base()
